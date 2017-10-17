5ee621a709080b2103e06d2fb5073a57afd98afb
tomasmaiorino <tomasmaiorino@gmail.com>
2017-10-17 00:54:29 -0200

    initial commit

---
+++ src/main/java/com/tst/rf/RfApplication.java
@@ -0,0 +1,12 @@
+package com.tst.rf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tst/rf/RfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tst.rf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
